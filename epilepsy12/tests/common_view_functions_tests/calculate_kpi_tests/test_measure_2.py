49e88cb3b49da580fd223f08b622d41396d1e3cc
eatyourpeas <simon@eatyourpeas.co.uk>
2025-01-05 14:57:34 +0000

    add tests for all options in kpi_2

--- epilepsy12/tests/common_view_functions_tests/calculate_kpi_tests/test_measure_2.py
+++ epilepsy12/tests/common_view_functions_tests/calculate_kpi_tests/test_measure_2.py
@@ -1,8 +1,21 @@
 """
 Measure 2 `epilepsy_specialist_nurse`
-- [x] Measure 2 passed (registration.kpi.epilepsy_specialist_nurse = 1) registration_instance.assessment.epilepsy_specialist_nurse_input_date <= first_paediatric_assessment_date + 1 year
-- [x] Measure 2 failed (registration.assessment.paediatrician_with_expertise_in_epilepsies = 0) if referral_made is False or input_date > first_paediatric_assessment_date + 1 year
-- [x] Measure 2 not_scored if incomplete (assessment.epilepsy_specialist_nurse_referral_made or assessment.epilepsy_specialist_nurse_input_date is None) NOTE: currently form cannot complete if both date fields are filled
+# - [x] Measure 2 passed (registration.kpi.epilepsy_specialist_nurse = 1) registration_instance.assessment.epilepsy_specialist_nurse_input_date <= first_paediatric_assessment_date + 1 year
+# - [x] Measure 2 failed (registration.assessment.paediatrician_with_expertise_in_epilepsies = 0) if referral_made is False or input_date > first_paediatric_assessment_date + 1 year
+# - [x] Measure 2 not_scored if incomplete (assessment.epilepsy_specialist_nurse_referral_made or assessment.epilepsy_specialist_nurse_input_date is None) NOTE: currently form cannot complete if both date fields are filled
+
+- [x] epilepsy_specialist_nurse_referral_made - True -> epilepsy_specialist_nurse_referral_date - PROVIDED -> epilepsy_specialist_nurse_input_achieved - TRUE -> epilepsy_specialist_nurse_input_date - NOT WITHIN FIRST YEAR OF CARE -> FAIL
+- [x] epilepsy_specialist_nurse_referral_made - True -> epilepsy_specialist_nurse_referral_date - PROVIDED -> (epilepsy_specialist_nurse_input_achieved - None) -> epilepsy_specialist_nurse_input_date - NOT WITHIN FIRST YEAR OF CARE -> FAIL (incomplete form but some users may have this as new field introduced late)
+- [x] epilepsy_specialist_nurse_referral_made - False -> FAIL
+
+- [x] epilepsy_specialist_nurse_referral_made - True -> epilepsy_specialist_nurse_referral_date - PROVIDED -> epilepsy_specialist_nurse_input_achieved - True -> epilepsy_specialist_nurse_input_date - WITHIN FIRST YEAR OF CARE -> PASS
+- [x] epilepsy_specialist_nurse_referral_made - True -> epilepsy_specialist_nurse_referral_date - PROVIDED -> (epilepsy_specialist_nurse_input_achieved - None) -> epilepsy_specialist_nurse_input_date - WITHIN FIRST YEAR OF CARE -> PASS (incomplete form but some users may have this as new field introduced late)
+- [x] epilepsy_specialist_nurse_referral_made - True -> epilepsy_specialist_nurse_referral_date - PROVIDED -> epilepsy_specialist_nurse_input_achieved - FALSE  (epilepsy_specialist_nurse_input_date NONE)-> PASS
+
+- [x] epilepsy_specialist_nurse_referral_made - None -> NOT_SCORED
+- [x] epilepsy_specialist_nurse_referral_made - True -> epilepsy_specialist_nurse_referral_date - None -> NOT_SCORED
+- [x] epilepsy_specialist_nurse_referral_made - True -> epilepsy_specialist_nurse_referral_date - PROVIDED -> epilepsy_specialist_nurse_input_achieved - None -> epilepsy_specialist_nurse_input_date - None -> NOT_SCORED
+- [x] epilepsy_specialist_nurse_referral_made - True -> epilepsy_specialist_nurse_referral_date - PROVIDED -> epilepsy_specialist_nurse_input_achieved - TRUE -> epilepsy_specialist_nurse_input_date - None -> NOT_SCORED
 
 Test Measure 2 - % of children and young people with epilepsy, with input by epilepsy specialist nurse within the first year of care
 
@@ -33,6 +46,7 @@ from epilepsy12.constants import KPI_SCORE
         (None, None, None, None, KPI_SCORE["NOT_SCORED"]),
         (True, None, None, None, KPI_SCORE["NOT_SCORED"]),
         (True, date(2023, 1, 1), None, None, KPI_SCORE["NOT_SCORED"]),
+        (True, date(2023, 1, 1), True, None, KPI_SCORE["NOT_SCORED"]),
     ],
 )
 @pytest.mark.django_db
@@ -111,7 +125,7 @@ def test_measure_2_should_fail_referral_after_1_yr(
     REFERRAL_DATE = FIRST_PAEDIATRIC_ASSESSMENT_DATE
     INPUT_DATE = FIRST_PAEDIATRIC_ASSESSMENT_DATE + relativedelta(years=1, days=1)
 
-    case = e12_case_factory(
+    case_achieved_scored = e12_case_factory(
         registration__first_paediatric_assessment_date=FIRST_PAEDIATRIC_ASSESSMENT_DATE,
         registration__assessment__epilepsy_specialist_nurse_referral_made=True,
         registration__assessment__epilepsy_specialist_nurse_referral_date=REFERRAL_DATE,
@@ -119,16 +133,37 @@ def test_measure_2_should_fail_referral_after_1_yr(
         registration__assessment__epilepsy_specialist_nurse_input_date=INPUT_DATE,
     )
 
+    case_achieved_unscored = e12_case_factory(
+        registration__first_paediatric_assessment_date=FIRST_PAEDIATRIC_ASSESSMENT_DATE,
+        registration__assessment__epilepsy_specialist_nurse_referral_made=True,
+        registration__assessment__epilepsy_specialist_nurse_referral_date=REFERRAL_DATE,
+        registration__assessment__epilepsy_specialist_nurse_input_achieved=None,
+        registration__assessment__epilepsy_specialist_nurse_input_date=INPUT_DATE,
+    )
+
     # get registration for the saved case model
-    registration = Registration.objects.get(case=case)
+    registration_achieved_scored = Registration.objects.get(case=case_achieved_scored)
+    registration_achieved_unscored = Registration.objects.get(
+        case=case_achieved_unscored
+    )
 
-    calculate_kpis(registration_instance=registration)
+    calculate_kpis(registration_instance=registration_achieved_scored)
+    calculate_kpis(registration_instance=registration_achieved_unscored)
 
-    kpi_score = KPI.objects.get(pk=registration.kpi.pk).epilepsy_specialist_nurse
+    achieved_scored_kpi_score = KPI.objects.get(
+        pk=registration_achieved_scored.kpi.pk
+    ).epilepsy_specialist_nurse
+    achieved_unscored_kpi_score = KPI.objects.get(
+        pk=registration_achieved_unscored.kpi.pk
+    ).epilepsy_specialist_nurse
 
     assert (
-        kpi_score == KPI_SCORE["FAIL"]
-    ), f"ESN Referral made 1y1day after first_paediatric_assessment_date but measure is not failing"
+        achieved_scored_kpi_score == KPI_SCORE["FAIL"]
+    ), f"ESN Referral made 1y1day after first_paediatric_assessment_date but measure is not failing (achieved field scored)"
+
+    assert (
+        achieved_unscored_kpi_score == KPI_SCORE["FAIL"]
+    ), f"ESN Referral made 1y1day after first_paediatric_assessment_date but measure is not failing (achieved field unscored)"
 
 
 @pytest.mark.django_db
@@ -145,7 +180,7 @@ def test_measure_2_should_pass_timely_input(
     PASSING_REFERRAL_DATE = FIRST_PAEDIATRIC_ASSESSMENT_DATE
     PASSING_INPUT_DATE = FIRST_PAEDIATRIC_ASSESSMENT_DATE + relativedelta(years=1)
 
-    case = e12_case_factory(
+    case_achieved_scored = e12_case_factory(
         registration__assessment__epilepsy_specialist_nurse_referral_made=True,
         registration__first_paediatric_assessment_date=FIRST_PAEDIATRIC_ASSESSMENT_DATE,
         registration__assessment__epilepsy_specialist_nurse_referral_date=PASSING_REFERRAL_DATE,
@@ -153,13 +188,49 @@ def test_measure_2_should_pass_timely_input(
         registration__assessment__epilepsy_specialist_nurse_input_date=PASSING_INPUT_DATE,
     )
 
+    case_achieved_unscored = e12_case_factory(
+        registration__assessment__epilepsy_specialist_nurse_referral_made=True,
+        registration__first_paediatric_assessment_date=FIRST_PAEDIATRIC_ASSESSMENT_DATE,
+        registration__assessment__epilepsy_specialist_nurse_referral_date=PASSING_REFERRAL_DATE,
+        registration__assessment__epilepsy_specialist_nurse_input_achieved=None,
+        registration__assessment__epilepsy_specialist_nurse_input_date=PASSING_INPUT_DATE,
+    )
+
+    case_achieved_false = e12_case_factory(
+        registration__assessment__epilepsy_specialist_nurse_referral_made=True,
+        registration__first_paediatric_assessment_date=FIRST_PAEDIATRIC_ASSESSMENT_DATE,
+        registration__assessment__epilepsy_specialist_nurse_referral_date=PASSING_REFERRAL_DATE,
+        registration__assessment__epilepsy_specialist_nurse_input_achieved=False,
+        registration__assessment__epilepsy_specialist_nurse_input_date=None,
+    )
+
     # get registration for the saved case model
-    registration = Registration.objects.get(case=case)
+    registration_achieved_scored = Registration.objects.get(case=case_achieved_scored)
+    registration_achieved_unscored = Registration.objects.get(
+        case=case_achieved_unscored
+    )
+    registration_achieved_false = Registration.objects.get(case=case_achieved_false)
 
-    calculate_kpis(registration_instance=registration)
+    calculate_kpis(registration_instance=registration_achieved_scored)
+    calculate_kpis(registration_instance=registration_achieved_unscored)
+    calculate_kpis(registration_instance=registration_achieved_false)
 
-    kpi_score = KPI.objects.get(pk=registration.kpi.pk).epilepsy_specialist_nurse
+    kpi_score_achieved_scored = KPI.objects.get(
+        pk=registration_achieved_scored.kpi.pk
+    ).epilepsy_specialist_nurse
+    kpi_score_achieved_unscored = KPI.objects.get(
+        pk=registration_achieved_unscored.kpi.pk
+    ).epilepsy_specialist_nurse
+    kpi_score_achieved_false = KPI.objects.get(
+        pk=registration_achieved_false.kpi.pk
+    ).epilepsy_specialist_nurse
 
     assert (
-        kpi_score == KPI_SCORE["PASS"]
-    ), f"Seen by epilepsy nurse within {PASSING_INPUT_DATE - FIRST_PAEDIATRIC_ASSESSMENT_DATE} but measure is not passing"
+        kpi_score_achieved_scored == KPI_SCORE["PASS"]
+    ), f"Seen by epilepsy nurse within {PASSING_INPUT_DATE - FIRST_PAEDIATRIC_ASSESSMENT_DATE} but measure is not passing (kpi_score_achieved_scored)"
+    assert (
+        kpi_score_achieved_unscored == KPI_SCORE["PASS"]
+    ), f"Seen by epilepsy nurse within {PASSING_INPUT_DATE - FIRST_PAEDIATRIC_ASSESSMENT_DATE} but measure is not passing (kpi_score_achieved_unscored)"
+    assert (
+        kpi_score_achieved_false == KPI_SCORE["PASS"]
+    ), f"Referred to epilepsy nurse on {PASSING_REFERRAL_DATE} and no second date but should pass as clinician declared seen date not achieved within the year. This should pass but measure is not passing"
