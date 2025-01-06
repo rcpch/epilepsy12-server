6010fa78f9041bfc82bad3f267e41b9e718abca7
eatyourpeas <simon@eatyourpeas.co.uk>
2025-01-06 20:36:56 +0000

    patient coordinates none causes scatterplot to fail, fixes distance min to max, adds tests for no location data Fixes #1150

--- epilepsy12/common_view_functions/filter_cases_for_geography.py
+++ epilepsy12/common_view_functions/filter_cases_for_geography.py
@@ -18,6 +18,9 @@ def filter_all_registered_cases_by_active_lead_site_and_cohort_and_level_of_abst
     filtered_cases = (
         Case.objects.filter(
             ~Q(postcode__in=UNKNOWN_POSTCODES_NO_SPACES),
+            location_wgs84__isnull=False,  # Ensure location is not null
+            site__organisation__longitude__isnull=False,  # Ensure location is not null
+            site__organisation__latitude__isnull=False,  # Ensure location is not null
             registration__isnull=False,
             registration__cohort=cohort,
             site__organisation=organisation,
