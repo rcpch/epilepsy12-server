8b65581c5467af41972cfa5dc5c6fa3d4a80bac5
Michael Barton <michael.barton@rcpch.ac.uk>
2025-01-07 14:34:52 +0000

    Format string instead

--- epilepsy12/general_functions/index_multiple_deprivation.py
+++ epilepsy12/general_functions/index_multiple_deprivation.py
@@ -30,7 +30,7 @@ def imd_for_postcode(user_postcode: str) -> int:
 
     if response.status_code != 200:
         logger.error(
-            f"Could not get deprivation score for {user_postcode}. Response status {response.status_code}"
+            "Could not get deprivation score for %s. Response status %s", user_postcode, response.status_code
         )
         return None
 
